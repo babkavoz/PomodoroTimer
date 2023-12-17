228bbc88f534beb1fdf2d3835586570fcf984f14
babkavoz <boryagirya@gmaiil.com>
2023-12-17 16:30:57 +0100

    add settings scrols

--- app/components/Settings.jsx
+++ app/components/Settings.jsx
@@ -1,10 +1,75 @@
 import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import React, { useState } from 'react'
+import { StyleSheet, Text, View, Button } from 'react-native'
+import { ScrollPicker } from 'react-native-value-picker'
+
+const generateMockData = count => {
+	const mockData = []
+	for (let i = 0; i <= count; i++) {
+		mockData.push({ value: i, label: `${i} minutes` })
+	}
+	return mockData
+}
+
+const MOCK_DATA = generateMockData(100)
+const MOCK_DATA2 = generateMockData(10)
 
 export default function Settings() {
+	const [pickedWorkTime, setPickedWorkTime] = useState(25)
+	const [pickedRestTime, setPickedRestTime] = useState(5)
+	const [pickedBigRestTime, setPickedBigRestTime] = useState(15)
+	const [pickedSessionCount, setPickedSessionCount] = useState(5)
+
 	return (
-		<View>
-			<Text>"Settings"</Text>
+		<View style={styles.container}>
+			<View style={styles.container}>
+				<Text>{'Work time'}</Text>
+				<ScrollPicker
+					currentValue={pickedWorkTime}
+					extraData={pickedWorkTime}
+					list={MOCK_DATA}
+					onItemPress={setPickedWorkTime}
+					labelColor='blue'
+					separatorColor='purple'
+					selectedColor='red'
+				/>
+			</View>
+			<View style={styles.container}>
+				<Text>{'Rest time'}</Text>
+				<ScrollPicker
+					currentValue={pickedRestTime}
+					extraData={pickedRestTime}
+					list={MOCK_DATA}
+					onItemPress={setPickedRestTime}
+					labelColor='blue'
+					separatorColor='purple'
+					selectedColor='red'
+				/>
+			</View>
+			<View style={styles.container}>
+				<Text>{'Big rest time'}</Text>
+				<ScrollPicker
+					currentValue={pickedBigRestTime}
+					extraData={pickedBigRestTime}
+					list={MOCK_DATA}
+					onItemPress={setPickedBigRestTime}
+					labelColor='blue'
+					separatorColor='purple'
+					selectedColor='red'
+				/>
+			</View>
+			<View style={styles.container}>
+				<Text>{'Session Count'}</Text>
+				<ScrollPicker
+					currentValue={pickedSessionCount}
+					extraData={pickedSessionCount}
+					list={MOCK_DATA2}
+					onItemPress={setPickedSessionCount}
+					labelColor='blue'
+					separatorColor='purple'
+					selectedColor='red'
+				/>
+			</View>
 		</View>
 	)
 }
@@ -12,8 +77,18 @@ export default function Settings() {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: '#fff',
+		backgroundColor: 'white',
 		alignItems: 'center',
-		justifyContent: 'center'
+		justifyContent: 'center',
+		height: 500
+	},
+	title: {
+		fontSize: 24,
+		marginBottom: 20
+	},
+	item: {
+		justifyContent: 'center',
+		alignItems: 'center',
+		height: 60
 	}
 })
